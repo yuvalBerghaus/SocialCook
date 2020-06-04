609bcf4b09fb372dd0ef0bb1c8c700f27533c44b
yuvalBerghaus <markberghaus@outlook.com>
2020-06-05 01:11:57 +0300

    v4.0.3

--- app/src/main/java/com/example/socialcook/afterlogin/MainPageFrag.java
+++ app/src/main/java/com/example/socialcook/afterlogin/MainPageFrag.java
@@ -38,6 +38,7 @@ public class MainPageFrag extends Fragment implements FireBase.IMainPage {
     private static ArrayList<Recipe> data;
     private static CustomAdapter adapter;
     static View.OnTouchListener myOnClickListener;
+    //:)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
