26dc0f1b224199bdf3de43b53b896ac4190ca709
yuvalBerghaus <markberghaus@outlook.com>
2020-06-14 17:44:11 +0300

    V9.2.6 -- Finally succeed with communication service!!!

--- app/src/main/java/com/example/socialcook/ReceiveNotificationActivity.java
+++ app/src/main/java/com/example/socialcook/ReceiveNotificationActivity.java
@@ -16,13 +16,16 @@ import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ReceiveNotificationActivity extends AppCompatActivity {
 
-    private static final String TAG = "recipeInRecievedNoti";
+    private static final String TAG = "DebuggingTogether";
     private static ArrayList<Recipe> data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +40,33 @@ public class ReceiveNotificationActivity extends AppCompatActivity {
             String brand = getIntent().getStringExtra("brandId");
             final String recipeName = getIntent().getStringExtra("recipeName");
             String recipeType = getIntent().getStringExtra("recipeType");
-            final Recipe[] recipe = new Recipe[0];
+            String recipe;
            categotyTv.setText(recipeType);
             brandTv.setText(recipeName);
+            myRef.orderByChild("recipeName").equalTo(recipeName).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for(DataSnapshot datas: dataSnapshot.getChildren()){
+                        String recipeNAme=datas.child("recipeName").getValue().toString();
+                        String recipeTYpe = datas.child("recipeType").getValue().toString();
+                        for (DataSnapshot child: datas.child("recipeAmount").getChildren()) {
+                            Log.d(TAG , child.getKey()+" = "+child.getValue());
+                        }
+                        for (DataSnapshot child: datas.child("recipeG").getChildren()) {
+                            Log.d(TAG , child.getKey()+" = "+child.getValue());
+                        }
+                        for (DataSnapshot child: datas.child("recipeML").getChildren()) {
+                            Log.d(TAG , child.getKey()+" = "+child.getValue());
+                        }
+                        Log.d(TAG , "recipe name = "+recipeNAme+"\nrecipe type = "+recipeTYpe);
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
         }
         /*
         if (getIntent().hasExtra("category")){
