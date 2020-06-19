1c47573627987a855ad753d60e2528783b17f469
yuvalBerghaus <markberghaus@outlook.com>
2020-06-20 02:20:08 +0300

    V9.6.3

--- app/src/main/java/com/example/socialcook/CustomAdapterIngridients.java
+++ app/src/main/java/com/example/socialcook/CustomAdapterIngridients.java
@@ -1,11 +1,14 @@
 package com.example.socialcook;
 
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.cardview.widget.CardView;
 import androidx.core.app.NotificationManagerCompat;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,6 +22,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Objects;
 
 public class CustomAdapterIngridients extends RecyclerView.Adapter<CustomAdapterIngridients.MyViewHolder>{
 
@@ -29,12 +33,14 @@ public class CustomAdapterIngridients extends RecyclerView.Adapter<CustomAdapter
         CardView cardView;
         TextView textViewName;
         Button infoButton;
+        TextView textInput;
         public MyViewHolder(View itemView) {
             super(itemView);
 
             this.cardView = (CardView) itemView.findViewById(R.id.cardView);
             this.textViewName = (TextView) itemView.findViewById(R.id.itemName);
             this.infoButton = (Button) itemView.findViewById(R.id.buttonSelectRoom);
+            this.textInput = (TextView) itemView.findViewById(R.id.itemValue);
         }
 
 
@@ -59,13 +65,17 @@ public class CustomAdapterIngridients extends RecyclerView.Adapter<CustomAdapter
 
         return myViewHolder;
     }
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public void onBindViewHolder(final MyViewHolder holder, final int listPosition) {
         String key = (new ArrayList<>(dataSet.keySet())).get(listPosition);
+        String value = Objects.requireNonNull(dataSet.get(key).toString());
         TextView textViewName = holder.textViewName;
+        TextView textInput = holder.textInput;
         CardView cardView = holder.cardView;
         final Button buttonInfo = holder.infoButton;
         textViewName.setText(key);
+        textInput.setText(value);
         cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
