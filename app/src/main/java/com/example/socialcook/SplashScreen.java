18b6c23db27a5503cb40749956e6d8c7aa2516fe
gituser <guy3317@gmail.com>
2020-09-20 10:54:08 +0300

    20/9/20 v11

--- app/src/main/java/com/example/socialcook/SplashScreen.java
+++ app/src/main/java/com/example/socialcook/SplashScreen.java
@@ -3,6 +3,7 @@ package com.example.socialcook;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashscreen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
