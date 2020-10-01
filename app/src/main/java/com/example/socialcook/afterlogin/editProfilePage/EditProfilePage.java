1feb50f34563711341c51732418eb7159b1b7213
gituser <guy3317@gmail.com>
2020-10-01 15:37:12 +0300

    edit profile page

--- app/src/main/java/com/example/socialcook/afterlogin/editProfilePage/EditProfilePage.java
+++ app/src/main/java/com/example/socialcook/afterlogin/editProfilePage/EditProfilePage.java
@@ -1,20 +1,41 @@
 package com.example.socialcook.afterlogin.editProfilePage;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.example.socialcook.R;
+import com.example.socialcook.beforelogin.MainActivity;
+import com.example.socialcook.classes.User;
 import com.example.socialcook.firebase.FireBase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,6 +43,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -29,6 +53,8 @@ import java.util.Locale;
 
 public class EditProfilePage extends Fragment {
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    StorageReference mStorageRef;
+    Uri image_uri;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
         final FirebaseDatabase database = FireBase.getDataBase();
@@ -37,10 +63,14 @@ public class EditProfilePage extends Fragment {
         final EditText nameInput = view.findViewById(R.id.nameInput);
         final EditText addressInput = view.findViewById(R.id.addressInput);
         final EditText birthdayInput = view.findViewById(R.id.birthdayInput);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
         Button nameButton = view.findViewById(R.id.nameSave);
         Button addressButton = view.findViewById(R.id.addressSave);
         Button birthdayButton = view.findViewById(R.id.birthdaySave);
         Button countryButton = view.findViewById(R.id.countrySave);
+        Button editPhotoButton = view.findViewById(R.id.editPhoto);
+        Button savePhotoButton = view.findViewById(R.id.savePhoto);
+        final ImageView editPhotoImage = view.findViewById(R.id.editPhotoImage);
         final Spinner editCountrySpinner = view.findViewById(R.id.editCountrySpinner);
 
         Locale[] locales = Locale.getAvailableLocales();
@@ -53,15 +83,47 @@ public class EditProfilePage extends Fragment {
         }
 
         Collections.sort(countries);
-        for (String country : countries) {
+        /*for (String country : countries) {
             System.out.println(country);
-        }
+        }*/
 
         ArrayAdapter<String> countryAdapter = new ArrayAdapter<String>(getActivity(), R.layout.spinner_item, countries);
 
         countryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the your spinner
         editCountrySpinner.setAdapter(countryAdapter);
+
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                final User user = dataSnapshot.getValue(User.class);
+                String imagePath = user.getImagePath();
+                if (imagePath != null) {
+                    FireBase.storageRef.child(imagePath).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            // Got the download URL for 'users/me/profile.png'
+                            Glide
+                                    .with(getContext())
+                                    .load(uri)
+                                    .centerCrop()
+                                    //.placeholder(progressBar.getProgressDrawable())
+                                    .into(editPhotoImage);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle any errors
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         userRef.child("name").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -144,7 +206,120 @@ public class EditProfilePage extends Fragment {
             }
         });
 
+        editPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+
+            }
+        });
+
+        savePhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        final User user = dataSnapshot.getValue(User.class);
+                        final StorageReference imagesRef = mStorageRef.child(user.getImagePath());
+                        Log.d("getimagepath", ""+user.getImagePath());
+                        imagesRef.putFile(image_uri)
+                                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot2) {
+                                        // Get a URL to the uploaded content
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception exception) {
+                                        // Handle unsuccessful uploads
+                                        // ...
+                                    }
+                                })
+                                .addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                        String downloadUrl = task.getResult().getStorage().getDownloadUrl().toString();
+                                        Log.d("downloadUrl", ""+downloadUrl);
+                                    }
+                                });
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+
+
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.main, menu);
+        super.onCreateOptionsMenu(menu,inflater);
+    }
+
+    public void selectImage() {
+        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Edit Photo!");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                if (options[item].equals("Take Photo"))
+                {
+                    ContentValues values = new ContentValues();
+                    values.put(MediaStore.Images.Media.TITLE, "New Picture");
+                    values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+                    image_uri = getActivity().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+                    startActivityForResult(cameraIntent, 1);
+                    /*Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+                    startActivityForResult(intent, 1); */
+                }
+                else if (options[item].equals("Choose from Gallery"))
+                {
+                    Intent intent = new   Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 2);
+                }
+                else if (options[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView editPhotoImage = (ImageView)getView().findViewById(R.id.editPhotoImage);
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            Log.d("IMAGECAPTURED", "IMAGECAPTURED!!");
+            if (requestCode == 1) {
+                editPhotoImage.setImageURI(image_uri);
+            }
+            else if (requestCode == 2) {
+                image_uri = data.getData();
+                String[] filePath = { MediaStore.Images.Media.DATA };
+                Context applicationContext = MainActivity.getContextOfApplication();
+                Cursor c = applicationContext.getContentResolver().query(image_uri,filePath, null, null, null);
+                c.moveToFirst();
+                int columnIndex = c.getColumnIndex(filePath[0]);
+                String picturePath = c.getString(columnIndex);
+                c.close();
+                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                editPhotoImage.setImageBitmap(thumbnail);
+            }
+        }
     }
 
 }
